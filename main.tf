09ab6e80604e997f5a9d855d93587aaaeb48c217
Devops <devopslearning223@gmail.com>
2026-08-19 23:20:44 +0530

    Remove Virtual Machine resources from main.tf

--- main.tf
+++ main.tf
@@ -88,46 +88,4 @@ resource "azurerm_subnet_nat_gateway_association" "vm_subnet_nat_assoc" {
   nat_gateway_id = azurerm_nat_gateway.nat_gw.id
 }
 
-# 11. Network Interface for VM (Private IP only)
-resource "azurerm_network_interface" "vm_nic" {
-  name                = "${var.vm_name}-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.vm_subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-
-  tags = var.tags
-}
-
-# 12. Linux Virtual Machine (Ubuntu 22.04 LTS)
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                            = var.vm_name
-  resource_group_name             = azurerm_resource_group.rg.name
-  location                        = azurerm_resource_group.rg.location
-  size                            = var.vm_size
-  admin_username                  = var.admin_username
-  admin_password                  = var.admin_password
-  disable_password_authentication = false
-
-  network_interface_ids = [
-    azurerm_network_interface.vm_nic.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-  tags = var.tags
-}
